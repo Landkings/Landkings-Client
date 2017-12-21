33cb753af18185edb6da5bdd0753764989e6cd9b
Zimovik007 <zimovik@list.ru>
2017-12-22 02:42:43 +1000

    sent code and nickname to the server

--- handler.php
+++ handler.php
@@ -1,9 +1,16 @@
 <?php
-
 	if (isset($_POST['submit'])){
-		$code = $_POST['code'];
-		$user = $_POST['user'];
-		echo $code.'\n'.$user.'\n';
+		$data = $_POST['code'];
+		$nickname = $_POST['user'];
+		
+		$ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://5.100.86.249:19997");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("nickname: $nickname"));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POST, 1); 
+        $result = curl_exec ($ch);
+        curl_close ($ch);
+        print $result;
 	}
 
 ?>
