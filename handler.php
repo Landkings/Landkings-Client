d373e1e08ce36c032e82ab09d9eae55460134801
Zimovik007 <zimovik@list.ru>
2017-12-23 13:09:21 +1000

    timeout handler.php 1 second

--- handler.php
+++ handler.php
@@ -7,7 +7,8 @@
         curl_setopt($ch, CURLOPT_URL, "http://10.193.31.203:19997");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("nickname: $nickname"));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_POST, 1); 
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
         $result = curl_exec ($ch);
         curl_close ($ch);
         print $result;
