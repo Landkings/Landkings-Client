7b9e879a7fe663aa2b2bf0487bac43d6d32d5171
Zimovik007 <zimovik@list.ru>
2017-12-23 13:28:06 +1000

    handler on main page

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+    if (isset($_POST['submit'])){
+        $data = $_POST['code'];
+        $nickname = $_POST['user'];
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:19997");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("nickname: $nickname"));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        $result = curl_exec ($ch);
+        curl_close ($ch);
+        print $result;
+    }
+
+?>
 <!doctype html5>
 <html>
 <head>
@@ -34,7 +51,7 @@
         </div>
         <div class="row">
             <div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-12">
-                <form class="form-group code-form" method="post" action="handler.php">
+                <form class="form-group code-form" method="post" action="index.php">
                     <label for="codeArea">Поле для ввода кода:</label>
                     <textarea id="codeArea" class="form-control" name="code"></textarea>
                     <label for="nickname">Имя персонажа:</label>
